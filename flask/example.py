05da9bb199acffe05948c5b3e434a7c7927638b8
Jeevan, Venkataramana <jeevan.venkataramana@gmail.com>
2019-02-18 21:41:58 -0800

    passing data to render template

--- flask/example.py
+++ flask/example.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+posts=[
+	{
+	'Name':'Jeevan Venkataramana',
+	'Email':'jeevan.venkataramana@gmail.com'
+	},
+	{
+	'Name':'Pavan',
+	'Email':'pavan.haravu@gmail.com'
+	}
+	]
 @app.route("/")
 def grant():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 @app.route("/viewrecords")
 def view():
